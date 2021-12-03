a3df38cd3dbdeb7b1ff0b2916c2bea688776e124
Tim Hallmann <tim@t8w.de>
2021-12-03 20:53:47 +0100

    Fix line counting
    Instead of skipping every other char (and thus possibly newlines), skip only immediately after a newline.

--- src/filechooser/filechooser.c
+++ src/filechooser/filechooser.c
@@ -54,8 +54,8 @@ static int exec_filechooser(void *data, bool writing, bool multiple, bool direct
         cr = getc(fp);
         if (cr == '\n') {
             num_lines++;
+            cr = getc(fp);
         }
-        cr = getc(fp);
         if (ferror(fp)) {
             return 1;
         }
